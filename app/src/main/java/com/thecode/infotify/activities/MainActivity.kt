0971e0a8ecd3aba54a2bfc06f01147d07baa2fb6
Gabriel Trésor TEK's <gabrielsuprematie@gmail.com>
2020-06-07 17:59:59 +0100

    :100: Improve Activities transitions & ViewPager animations
    - Change Windows transitions to Fading
    - Change Slide animations of ViewPager to Fading

--- app/src/main/java/com/thecode/infotify/activities/MainActivity.kt
+++ app/src/main/java/com/thecode/infotify/activities/MainActivity.kt
@@ -3,17 +3,21 @@ package com.thecode.infotify.activities
 import android.content.Context
 import android.graphics.Typeface
 import android.os.Bundle
+import android.view.View
 import androidx.annotation.StyleRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import com.thecode.infotify.R
 import com.thecode.infotify.adapters.BottomNavPagerAdapter
 import com.thecode.infotify.fragments.BookmarksFragment
 import com.thecode.infotify.fragments.HomeFragment
 import com.thecode.infotify.fragments.SearchFragment
+import com.thecode.infotify.interfaces.FadePageTransformer
 import com.thecode.infotify.utils.SharedPreferenceUtils
 import io.github.inflationx.viewpump.ViewPumpContextWrapper
 import kotlinx.android.synthetic.main.activity_main_2.*
+import java.text.FieldPosition
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -36,9 +40,7 @@ class MainActivity : AppCompatActivity() {
 
         val bnlv = bottom_navigation_bar
         bnlv.setTypeface(Typeface.createFromAsset(assets, "fonts/SF-Medium.otf"))
-        //bnlv.setBadgeValue(0, "3");
-        //bnlv.setBadgeValue(1, "9+"); //invisible badge
-        //bnlv.setBadgeValue(0, "9+")
+       //bnlv.setBadgeValue(0, "9+")
 
         val fragList = ArrayList<Fragment>()
         fragList.add(HomeFragment())
@@ -48,6 +50,7 @@ class MainActivity : AppCompatActivity() {
         val viewPager = view_pager
         viewPager.offscreenPageLimit = 3
         viewPager.adapter = pagerAdapter
+        viewPager.setPageTransformer(false, FadePageTransformer())
 
         bnlv.setNavigationChangeListener { _, position ->
             viewPager.setCurrentItem(
